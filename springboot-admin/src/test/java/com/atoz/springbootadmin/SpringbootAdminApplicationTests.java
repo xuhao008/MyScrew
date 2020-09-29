8967a263a8e3ae934a9522fa9cd0d6f93422bd77
xuhao <xhxxswkl@163.com>
2020-09-29 16:19:39 +0800

    springboot-admin

---
+++ springboot-admin/src/test/java/com/atoz/springbootadmin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoz.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
